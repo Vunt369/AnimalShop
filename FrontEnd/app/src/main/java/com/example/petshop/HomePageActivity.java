c1be9fe37a1d1ba8e7d00cc36cbdd5d0e7504164
Duong Thi Hien <134600777+DuongHienne@users.noreply.github.com>
2024-07-05 01:07:52 +0700

    list_product_homepage

--- FrontEnd/app/src/main/java/com/example/petshop/HomePageActivity.java
+++ FrontEnd/app/src/main/java/com/example/petshop/HomePageActivity.java
@@ -8,9 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.petshop.Products.Product;
+import com.example.petshop.Products.ProductAdapter;
 import com.example.petshop.categories.Category;
 import com.example.petshop.categories.CategoryAdapter;
 
@@ -19,7 +22,7 @@ import java.util.ArrayList;
 public class HomePageActivity extends AppCompatActivity {
 
     ArrayList<Category> categoriesList;
-
+    ArrayList<Product> productsList;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -38,6 +41,24 @@ public class HomePageActivity extends AppCompatActivity {
         CategoryAdapter adapter = new CategoryAdapter(categoriesList);
         rvCategories.setAdapter(adapter);
         rvCategories.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        RecyclerView rvProducts = findViewById(R.id.recycler_products);
+        productsList = new ArrayList<>();
+        productsList.add(new Product(R.drawable.yem_co, "Yếm cổ đáng yêu", 35000));
+        productsList.add(new Product(R.drawable.thuc_an_hat_kho, "Thức ăn hạt khô", 230000));
+        productsList.add(new Product(R.drawable.sua_tam_joyce, "Sữa tắm JOYCR&DOLCE", 175000));
+        productsList.add(new Product(R.drawable.bat_an_nghieng, "Bát ăn nghiêng", 45000));
+        productsList.add(new Product(R.drawable.banh_thuong_catnipo, "Bánh thưởng CATNIP", 30000));
+        productsList.add(new Product(R.drawable.vong_co_kem_chuong, "Vòng cổ kèm chuông", 35000));
+        productsList.add(new Product(R.drawable.balo_cho_meo, "Balô cho mèo", 200000));
+        productsList.add(new Product(R.drawable.bo_do_choi_cho_meo, "Bộ đồ chơi cho mèo", 200000));
+
+        ProductAdapter adapterProduct = new ProductAdapter(productsList);
+        rvProducts.setAdapter(adapterProduct);
+        rvProducts.setLayoutManager(new GridLayoutManager(this, 2));
+
+
+
     }
 
 
